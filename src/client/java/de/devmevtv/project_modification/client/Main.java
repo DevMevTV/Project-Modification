c7f3eb788c210b9d423eb6f21cb77e2affed83cf
DevMevTV <devidfitz533@gmail.com>
2025-01-10 16:38:30 +0100

    Init

---
+++ src/client/java/de/devmevtv/project_modification/client/Main.java
@@ -0,0 +1,11 @@
+package de.devmevtv.project_modification.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Main implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+        System.out.println("AAAAAAA MOD IS ONLINE :O");
+    }
+}
---
+++ src/main/java/de/devmevtv/project_modification/Main.java
@@ -0,0 +1,10 @@
+package de.devmevtv.project_modification;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Main implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
