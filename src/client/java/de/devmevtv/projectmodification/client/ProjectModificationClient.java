a5231b8ea5080973adb4e99bd6170fe2832d8cfa
DevMevTV <devidfitz533@gmail.com>
2025-01-11 00:53:15 +0100

    owo ig idk

--- src/client/java/de/devmevtv/projectmodification/client/ProjectModificationClient.java
+++ src/client/java/de/devmevtv/projectmodification/client/ProjectModificationClient.java
@@ -4,8 +4,12 @@ import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
 import net.fabricmc.fabric.api.client.message.v1.ClientReceiveMessageEvents;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.text.Text;
 
 public class ProjectModificationClient implements ClientModInitializer {
+
+    public static boolean PMOD;
+    public static int PermissionLevel;
 
     @Override
     public void onInitializeClient() {
@@ -14,13 +18,25 @@ public class ProjectModificationClient implements ClientModInitializer {
         }));
 
         ClientReceiveMessageEvents.ALLOW_GAME.register((message, overlay) -> {
-            if (message.getString().startsWith("ȐУȴфঙ")
-                    || message.getString().equals("Unknown scoreboard objective 'pmod.handshake'")
-                    || message.getString().equals("Triggered [pmod.handshake]")) {
+            if (message.getString().equals("Unknown scoreboard objective 'pmod.handshake'")) {
+                PMOD = false;
                 return false;
-            } else {
-                return true;
+            } else if (message.getString().equals("Triggered [pmod.handshake]")) {
+                PMOD = true;
+                return false;
             }
+
+            if (message.getString().startsWith("ȐУȴфঙ")) {
+                String msg = message.getString().substring(5);
+
+                if (msg.startsWith("LVL_")) {
+                    this.PermissionLevel = Integer.parseInt(msg.substring(4));
+                    MinecraftClient.getInstance().player.sendMessage(Text.literal("You have permission level " + this.PermissionLevel), true);
+                }
+                return false;
+            }
+
+            return true;
         });
     }
 }
