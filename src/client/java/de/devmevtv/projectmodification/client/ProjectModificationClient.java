972209f06b8aa2ea3b4235bd2b7decbdd03076cf
Omri H <omri.hermon@gmail.com>
2025-01-11 01:55:08 +0200

    oops

--- src/client/java/de/devmevtv/projectmodification/client/ProjectModificationClient.java
+++ src/client/java/de/devmevtv/projectmodification/client/ProjectModificationClient.java
@@ -8,8 +8,6 @@ import net.minecraft.client.MinecraftClient;
 import net.minecraft.text.Text;
 
 public class ProjectModificationClient implements ClientModInitializer {
-    public int permissionLevel = 0;
-
     public static boolean PMOD;
     public static int PermissionLevel;
 
@@ -19,7 +17,7 @@ public class ProjectModificationClient implements ClientModInitializer {
             MinecraftClient.getInstance().player.networkHandler.sendChatCommand("trigger pmod.handshake");
         }));
         ClientPlayConnectionEvents.DISCONNECT.register((clientPlayNetworkHandler, minecraftClient) -> {
-            permissionLevel = 0;
+            PermissionLevel = 0;
         });
 
         ClientReceiveMessageEvents.ALLOW_GAME.register((message, overlay) -> {
@@ -35,20 +33,13 @@ public class ProjectModificationClient implements ClientModInitializer {
                 String msg = message.getString().substring(5);
 
                 if (msg.startsWith("LVL_")) {
-                    this.PermissionLevel = Integer.parseInt(msg.substring(4));
-                    MinecraftClient.getInstance().player.sendMessage(Text.literal("You have permission level " + this.PermissionLevel), true);
+                    PermissionLevel = Integer.parseInt(msg.substring(4));
+                    MinecraftClient.getInstance().player.sendMessage(Text.literal("You have permission level " + PermissionLevel), true);
                 }
                 return false;
             }
 
             return true;
-        });
-        ClientReceiveMessageEvents.GAME.register((message, overlay) -> {
-            if (message.getString().equals("ȐУȴфঙLVL_1")) {
-                permissionLevel = 1;
-            } else if (message.getString().equals("ȐУȴфঙLVL_0")) {
-                permissionLevel = 0;
-            }
         });
     }
 }
