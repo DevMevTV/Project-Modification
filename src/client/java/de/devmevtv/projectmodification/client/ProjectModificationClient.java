47057273d1153a94485d271f86df2013131d33c8
Omri H <omri.hermon@gmail.com>
2025-01-10 17:53:48 +0200

    stuff

---
+++ src/client/java/de/devmevtv/projectmodification/client/ProjectModificationClient.java
@@ -0,0 +1,10 @@
+package de.devmevtv.projectmodification.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ProjectModificationClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/de/devmevtv/projectmodification/ProjectModification.java
@@ -0,0 +1,10 @@
+package de.devmevtv.projectmodification;
+
+import net.fabricmc.api.ModInitializer;
+
+public class ProjectModification implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
