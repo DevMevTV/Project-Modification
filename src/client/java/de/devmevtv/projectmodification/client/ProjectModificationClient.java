7e53571879292ac0129f0603440ff3df630be94b
DevMevTV <devidfitz533@gmail.com>
2025-01-10 22:57:58 +0100

    cdefgah

--- src/client/java/de/devmevtv/projectmodification/client/ProjectModificationClient.java
+++ src/client/java/de/devmevtv/projectmodification/client/ProjectModificationClient.java
@@ -1,10 +1,17 @@
 package de.devmevtv.projectmodification.client;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
 
 public class ProjectModificationClient implements ClientModInitializer {
 
     @Override
     public void onInitializeClient() {
+        ClientPlayConnectionEvents.JOIN.register((handler, sender, client) -> {
+            ClientCommandRegistrationCallback.EVENT.register((dispatcher, access) -> {
+                handler.sendCommand("trigger test");
+            });
+        });
     }
 }
